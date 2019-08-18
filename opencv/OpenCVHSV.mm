a85f06b1a0f9b5c800fd60b2ef7c494d4b863d2b
JMarshall <13102167578@163.com>
2019-08-18 12:58:31 +0800

    修改9和6这种带短杠的数字的检测逻辑

--- opencv/OpenCVHSV.mm
+++ opencv/OpenCVHSV.mm
@@ -31,6 +31,9 @@ vector<Vec4i> hierarchy;
 
 vector<vector<cv::Point>> fakeContours;
 
+Mat temp_element_1;
+Mat temp_imgThresholded;
+Mat imgThresholded;
 @implementation OpenCVHSV
 - (void)isThisWorking{
     
@@ -42,13 +45,13 @@ vector<vector<cv::Point>> fakeContours;
 {
     outputSize = 300;
     //Blue 789
-     iLowH = 105;
+     iLowH = 115;
      iHighH = 130;
      iLowS = 90;
      iHighS = 255;
      iLowV = 0;
      iHighV = 255;
-    
+     temp_element_1 = getStructuringElement(MORPH_RECT, cv::Size(30, 30));
     //Red 0213
     //iLowH = 170;
     //iHighH = 179;
@@ -59,7 +62,7 @@ vector<vector<cv::Point>> fakeContours;
     
     self.videoCamera = [[CvVideoCamera alloc] init];
     self.videoCamera.delegate = self;
-    self.videoCamera.defaultAVCaptureDevicePosition =AVCaptureDevicePositionBack;
+    self.videoCamera.defaultAVCaptureDevicePosition =AVCaptureDevicePositionFront;
     self.videoCamera.defaultAVCaptureSessionPreset =AVCaptureSessionPresetHigh;
     self.videoCamera.defaultAVCaptureVideoOrientation = AVCaptureVideoOrientationPortrait;
     self.videoCamera.defaultFPS = 5;
@@ -81,31 +84,40 @@ vector<vector<cv::Point>> fakeContours;
     split(imgHSV, hsvSplit);
     equalizeHist(hsvSplit[2],hsvSplit[2]);
     merge(hsvSplit,imgHSV);
-    Mat imgThresholded;
+    
     
     inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
-    
-    
+
     //开操作 (去除一些噪点)
-    Mat element = getStructuringElement(MORPH_RECT, cv::Size(5, 5));
-    morphologyEx(imgThresholded, imgThresholded, MORPH_OPEN, element);
-    
-    //闭操作 (连接一些连通域)
-    morphologyEx(imgThresholded, imgThresholded, MORPH_CLOSE, element);
-    
-    
+//    Mat element = getStructuringElement(MORPH_RECT, cv::Size(5, 5));
+//    morphologyEx(imgThresholded, imgThresholded, MORPH_OPEN, element);
+//
+//    //闭操作 (连接一些连通域)
+//    morphologyEx(imgThresholded, imgThresholded, MORPH_CLOSE, element);
+
     if (!imgThresholded.empty()) {
-        findContours(imgThresholded, contours, hierarchy, RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+        imgThresholded.copyTo(temp_imgThresholded);
+        //Mat temp_element = getStructuringElement(MORPH_RECT, cv::Size(5, 5));
+        //morphologyEx(temp_imgThresholded, temp_imgThresholded, MORPH_OPEN, temp_element);
+        morphologyEx(temp_imgThresholded, temp_imgThresholded, MORPH_CLOSE, temp_element_1);
+
+        findContours(temp_imgThresholded, contours, hierarchy, RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+        
+//        findContours(imgThresholded, contours, hierarchy, RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
         
         CvRect rect;
-        fakeContours = MatGetAreaMaxContour(contours);
+        //fakeContours = MatGetAreaMaxContour(contours);
+        fakeContours = MatGetQualifiedAreaContour(contours);
+
+        
         NSMutableArray* RectImageArray = [[NSMutableArray alloc] init];
         
         if (!contours.empty() && !fakeContours.empty()) {
-            //  cout<<"识别到的数量:"<<fakeContours.size()<<endl;
+              cout<<"识别到的数量:"<<fakeContours.size()<<endl;
             
             for (int i = 0; i < fakeContours.size(); i++) {
-                rect = boundingRect(MatGetAreaMaxContour(contours)[i]);
+                rect = boundingRect(fakeContours[i]);
                 Mat ROI = imgThresholded(rect);
                // NSLog(@"1111111111111111:%d,%d",ROI.cols,ROI.rows);
                 Mat temp;
@@ -138,7 +150,9 @@ vector<vector<cv::Point>> fakeContours;
         if ([_delegate respondsToSelector:@selector(rectImageDidProcessed:)]) {
             [_delegate rectImageDidProcessed:RectImageArray];
         }
-        
+        if ([_delegate respondsToSelector:@selector(imageDidProcessed:)]) {
+            [_delegate imageDidProcessed:MatToUIImage(imgThresholded)];
+        }
     }
     
 //    dispatch_async(dispatch_get_main_queue(), ^{
@@ -147,9 +161,9 @@ vector<vector<cv::Point>> fakeContours;
 //
 //    });
 //    _resultImage = MatToUIImage(imgThresholded);
-    if ([_delegate respondsToSelector:@selector(imageDidProcessed:)]) {
-        [_delegate imageDidProcessed:MatToUIImage(imgThresholded)];
-    }
+//    if ([_delegate respondsToSelector:@selector(imageDidProcessed:)]) {
+//        [_delegate imageDidProcessed:MatToUIImage(temp_imgThresholded)];
+//    }
     contours.clear();
 
 }
@@ -173,63 +187,25 @@ vector<vector<cv::Point>> MatGetAreaMaxContour(vector<vector<cv::Point>> contour
     return area_max_contour;
 }
 
-- (void)HSFprocessImage:(UIImage*) imageUIImage
-{
-    // 将图像转换为灰度显示
-    //cvtColor(image, image, COLOR_RGBA2RGB);
-    Mat image;
-    UIImageToMat(imageUIImage, image);
-    
-    Mat imgHSV;
-    vector<Mat> hsvSplit;
-    cvtColor(image, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
-    
-    //因为我们读取的是彩色图，直方图均衡化需要在HSV空间做
-    split(imgHSV, hsvSplit);
-    equalizeHist(hsvSplit[2],hsvSplit[2]);
-    merge(hsvSplit,imgHSV);
-    Mat imgThresholded;
-    
-    inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
-    
-    
-    //开操作 (去除一些噪点)
-    Mat element = getStructuringElement(MORPH_RECT, cv::Size(5, 5));
-    morphologyEx(imgThresholded, imgThresholded, MORPH_OPEN, element);
-    
-    //闭操作 (连接一些连通域)
-    morphologyEx(imgThresholded, imgThresholded, MORPH_CLOSE, element);
-    
-    
-    if (!imgThresholded.empty()) {
-        findContours(imgThresholded, contours, hierarchy, RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+vector<vector<cv::Point>> MatGetQualifiedAreaContour(vector<vector<cv::Point>> contour)
+{//在给定的contour中找到面积最大的一个轮廓，并返回指向该轮廓的指针
+
+    vector<vector<cv::Point>> area_qualified_contour ;//指向面积最大的轮廓
+    double contour_area_temp=0;
+    for(int i = 0 ; i < contour.size();i++)
+    {//寻找面积最大的轮廓，即循环结束时的area_max_contour
+        contour_area_temp = fabs(contourArea(contour[i])); //获取当前轮廓面积
         
-        CvRect rect;
-        fakeContours = MatGetAreaMaxContour(contours);
+        NSLog(@"d面积是:%f",contour_area_temp);
         
-        if (!contours.empty() && !fakeContours.empty()) {
-            //  cout<<"识别到的数量:"<<fakeContours.size()<<endl;
-            
-            for (int i = 0; i < fakeContours.size(); i++) {
-                rect = boundingRect(MatGetAreaMaxContour(contours)[i]);
-                Mat ROI = imgThresholded(rect);
-            }
+        if( contour_area_temp > 1000)
+        {
+            area_qualified_contour.push_back(contour[i]);//记录面积最大的轮廓
         }
-        
     }
     
-    //    dispatch_async(dispatch_get_main_queue(), ^{
-    //
-    //        self.testView.image = MatToUIImage(imgThresholded);
-    //
-    //    });
-    
-    contours.clear();
-    if ([_delegate respondsToSelector:@selector(imageDidProcessed:)]) {
-        [_delegate imageDidProcessed:MatToUIImage(imgThresholded)];
-    }
+    return area_qualified_contour;
 }
-   // return  MatToUIImage(imgThresholded);
 
 -(void) SetiLowH:(int) i{
     iLowH = i;
